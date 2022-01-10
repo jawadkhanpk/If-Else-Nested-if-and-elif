6a06f87b4a22e50194d0299d09eb880f4dc29500
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-10 12:30:41 +0500

    added another if

--- nested-if-and-elif-statments.py
+++ nested-if-and-elif-statments.py
@@ -1,18 +1,27 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
+bill = 0
+
 if height >= 120:
     print("you can ride the rollercoaster")
 
     age = int(input("What is your age"))
 
     if age <=12:                            # nested-if start
-        print("You need to pay $5")
+        bill = 5
+        print("Child Tickets are $5")
     elif age <=18:
-        print("You need to pay $7")
+        bill = 18
+        print("Youth Tickets are $7")
     else:
-        print("you need to pay $12")        # nested-if ended
+        bill = 12
+        print("Adults Tickets are $12")        # nested-if ended
 
+    wants_photo = input("Do you want a photo taken? Y or N: ")
+    if wants_photo == "Y":
+        bill += 3
+    print(f"your final bill is ${bill}")
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
